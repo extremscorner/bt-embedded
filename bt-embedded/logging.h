2e8f42bb314c534f126f923943e89f68b9dc035a
Alberto Mardegan <mardy@users.sourceforge.net>
2026-03-12 18:41:14 +0300

    build: add a cmake option to enable/disable debug logs

--- bt-embedded/logging.h
+++ bt-embedded/logging.h
@@ -10,19 +10,18 @@
 extern "C" {
 #endif
 
-#define DEBUG 1
-
 /* BTE_LOG might have been defined in platform_defs.h */
 #ifndef BTE_LOG
 #define BTE_LOG(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
 #endif
 
 #define BTE_WARN(fmt, ...) BTE_LOG("[E] " fmt "\n", ##__VA_ARGS__)
-#define BTE_INFO(fmt, ...) BTE_LOG("[I] " fmt "\n", ##__VA_ARGS__)
-#if DEBUG
+#ifdef WITH_DEBUG
 #define BTE_DEBUG(fmt, ...) BTE_LOG("[D] %s:%d: " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#define BTE_INFO(fmt, ...) BTE_LOG("[I] " fmt "\n", ##__VA_ARGS__)
 #else
 #define BTE_DEBUG(fmt, ...) (void)0
+#define BTE_INFO(fmt, ...) (void)0
 #endif
 
 #ifdef __cplusplus
