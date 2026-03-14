3946c178ce91cd2baeea81176ed19abc84f997bb
Alberto Mardegan <mardy@users.sourceforge.net>
2026-03-14 10:38:09 +0300

    types: add helper functions for class of device

--- bt-embedded/types.h
+++ bt-embedded/types.h
@@ -26,6 +26,71 @@ typedef struct {
     uint8_t bytes[3];
 } BteClassOfDevice;
 
+static inline uint16_t bte_cod_get_service_class(BteClassOfDevice cod) {
+    return (cod.bytes[2] << 3) | (cod.bytes[1] >> 5);
+}
+
+static inline uint8_t bte_cod_get_major_dev_class(BteClassOfDevice cod) {
+    return cod.bytes[1] & 0x1f;
+}
+
+static inline uint8_t bte_cod_get_minor_dev_class(BteClassOfDevice cod) {
+    return cod.bytes[0] >> 2;
+}
+
+static inline BteClassOfDevice bte_cod_compose(
+    uint16_t service_class, uint8_t major_dev_class, uint8_t minor_dev_class) {
+    BteClassOfDevice cod = {{
+        (uint8_t)(minor_dev_class << 2),
+        (uint8_t)(major_dev_class | (service_class << 5)),
+        (uint8_t)(service_class >> 3),
+    }};
+    return cod;
+}
+
+/** Limited Discoverable Mode */
+#define BTE_COD_SERVICE_CLASS_LDM       (uint16_t)(1 << 0)
+#define BTE_COD_SERVICE_CLASS_LE_AUDIO  (uint16_t)(1 << 1) /**< LE audio */
+/** Positioning (local identification) */
+#define BTE_COD_SERVICE_CLASS_POSITION  (uint16_t)(1 << 3)
+/** Networking (LAN, Ad hoc, ...) */
+#define BTE_COD_SERVICE_CLASS_NETWORK   (uint16_t)(1 << 4)
+/** Rendering (Printing, Speakers, ...) */
+#define BTE_COD_SERVICE_CLASS_RENDER    (uint16_t)(1 << 5)
+/** Capturing (Scanner, Microphone, ...) */
+#define BTE_COD_SERVICE_CLASS_CAPTURE   (uint16_t)(1 << 6)
+/** Object Transfer (v-Inbox, v-Folder, ...) */
+#define BTE_COD_SERVICE_CLASS_TRANSFER  (uint16_t)(1 << 7)
+/** Audio (Speaker, Microphone, Headset service, ...) */
+#define BTE_COD_SERVICE_CLASS_AUDIO     (uint16_t)(1 << 8)
+/** Telephony (Cordless telephony, Modem, Headset service, ...) */
+#define BTE_COD_SERVICE_CLASS_TELEPHONY (uint16_t)(1 << 9)
+/** Information (WEB-server, WAP-server, ...) */
+#define BTE_COD_SERVICE_CLASS_INFO      (uint16_t)(1 << 10)
+
+/** Miscellaneous */
+#define BTE_COD_MAJOR_DEV_CLASS_MISC     (uint8_t)0x0
+/** Computer (desktop, notebook, PDA, organizer, ...) */
+#define BTE_COD_MAJOR_DEV_CLASS_COMPUTER (uint8_t)0x1
+/** Phone (cellular, cordless, pay phone, modem, ...) */
+#define BTE_COD_MAJOR_DEV_CLASS_PHONE    (uint8_t)0x2
+/** LAN/Network Access Point */
+#define BTE_COD_MAJOR_DEV_CLASS_NETWORK  (uint8_t)0x3
+/** Audio/Video (headset, speaker, stereo, video display, VCR, ...) */
+#define BTE_COD_MAJOR_DEV_CLASS_AV       (uint8_t)0x4
+/** Peripheral (mouse, joystick, keyboard, ...) */
+#define BTE_COD_MAJOR_DEV_CLASS_PERIPH   (uint8_t)0x5
+/** Imaging (printer, scanner, camera, display, ...) */
+#define BTE_COD_MAJOR_DEV_CLASS_IMAGING  (uint8_t)0x6
+/** Wearable */
+#define BTE_COD_MAJOR_DEV_CLASS_WEAR     (uint8_t)0x7
+/** Toy */
+#define BTE_COD_MAJOR_DEV_CLASS_TOY      (uint8_t)0x8
+/** Health */
+#define BTE_COD_MAJOR_DEV_CLASS_HEALTH   (uint8_t)0x9
+/** Uncategorized (device code not specified) */
+#define BTE_COD_MAJOR_DEV_CLASS_UNCAT    (uint8_t)0x1f
+
 typedef struct {
     uint8_t bytes[16];
 } BteLinkKey;
